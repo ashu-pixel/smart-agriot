fa3f4e515386782cc3df75adb440f16817329429
ashu-pixel <ashutoshgupta1704@gmail.com>
2023-03-06 19:17:27 +0530

    role based distributedsystem

--- APIS/app.py
+++ APIS/app.py
@@ -1,58 +1,88 @@
-from flask import Flask,jsonify,request,make_response
-import pickle
 import numpy as np
+import pickle
+from flask import Flask, request, jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 # Load model from model.pkl
-Wmodel = pickle.load(open('weather_prediction.pkl', 'rb'))
-Cmodel = pickle.load(open('model.pkl', 'rb'))
+Wmodel = pickle.load(open('./models/weather_prediction.pkl', 'rb'))
+Cmodel = pickle.load(open('./models/crop_prediction.pkl', 'rb'))
 
-@app.route('/' )
+@app.route('/' , methods=['GET'])
 def hm():
-    response = "HELLO WORLD from SMART-AgrIoT"
-    resp = {"ans" : response } 
-    return jsonify(resp)
+    return jsonify({"ans": "HELLO WORLD from SMART-AgrIoT"})
 
-
-@app.route('/weatherPred',methods = ['POST', 'GET'])
-def predict():
-    if request.method == 'POST':
-        content_type = request.headers.get('Content-Type')
-        print("content_type",content_type)
-        if (content_type == 'application/json'):
-            data = request.json
-            params = ["temp", "humi", "windS", "press" , "windD" , "visibility" ]
-
-            int_features = [float(data[x]) for x in params]
-            print(int_features)
-            final_features = [np.array(int_features)]
-            prediction = Wmodel.predict(final_features)
-            output = prediction[0]
-            print(output)
-            ans = {"val" : output}
-            return jsonify(ans)
-        else : return 'bad request!', 400
-    else : return 'bad request!', 400
-
-
-
-@app.route('/CropPred',methods = ['POST', 'GET'])
+@app.route('/CropPred', methods=['POST'])
 def pres():
-    if request.method == 'POST':
-        content_type = request.headers.get('Content-Type')
-        print(content_type)
-        if (content_type == 'application/json'):
-            data = request.json
-            params = ["season", "temp", "humi", "rainfall", "soil" ]
-            int_features = [float(data[x]) for x in params]     
-            final_features = [np.array(int_features)]
-            prediction = Cmodel.predict(final_features)
-            output = prediction[0]
-            print(output)
-            ans = {"val" : output}
-            return jsonify(ans)
-        else : return 'bad request!', 400
+    content_type = request.headers.get('Content-Type')
+    
+    if (content_type == 'application/json'):
+        incomingData = request.json
+
+        cr = incomingData["marketVals"]
+        data = incomingData["formVals"]
+        final_features = [np.array(data)]
+
+        pred_proba = Cmodel.predict_proba(final_features)
+
+        res = {}
+        crops = {'apple': 0, 'banana': 1, 'blackgram': 2, 'chickpea': 3, 'coconut': 4, 'coffee': 5,
+                 'cotton': 6, 'grapes': 7, 'jute': 8, 'kidneybeans': 9, 'lentil': 10, 'maize': 11,
+                 'mango': 12, 'mothbeans': 13, 'mungbean': 14, 'muskmelon': 15, 'orange': 16, 'papaya': 17,
+                 'pigeonpeas': 18, 'pomegranate': 19, 'rice': 20, 'watermelon': 21}
+
+        keys = list(crops.keys())
+        values = list(crops.values())
+
+        for i in range(len(pred_proba[0])):
+            if(pred_proba[0][i] > 0):
+                res[i] = pred_proba[0][i]
+
+        keys1 = list(res.keys())
+        values1 = list(res.values())
+
+        sorted_value_index = np.argsort(values1)
+        sorted_dict = {keys1[i]: values1[i] for i in sorted_value_index}
+        sorted_dict = {keys[values.index(keys1[i])]: values1[i]
+                       for i in sorted_value_index}
+
+        final_op = {}
+        for key in sorted_dict:
+            if key in cr:
+                final_op[key] = sorted_dict[key] * cr[key]
+
+        top_classes = []
+        for x in list(reversed(list(final_op))):
+            top_classes.append(x)
+
+        response = jsonify({
+            "statusCode": 200,
+            "status": "Prediction made",
+            "result": top_classes[0]
+        })
+
+        return jsonify({"res1": top_classes[0], "res2": top_classes[1], "res3": top_classes[2] })
+
+    else:
+        return 'bad request!', 400
 
 
-if __name__=="__main__":
+@app.route('/weatherPred', methods=['POST' ])
+def predict():
+    content_type = request.headers.get('Content-Type')
+     
+    if (content_type == 'application/json'):
+
+        data = request.json
+        final_features = [np.array(data["formVals"])]
+         
+        prediction = Wmodel.predict(final_features)
+        output = prediction[0]
+        ans = {"val": output}
+
+        return jsonify(ans)
+    else:
+        return 'bad request!', 400
+
+if __name__ == "__main__":
     app.run(debug=True)
--- WEB/app.js
+++ WEB/app.js
@@ -5,19 +5,29 @@ const fetch = require('node-fetch');
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 const app = express()
+var admin = require("firebase-admin");
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
+app.use(express.static(__dirname + "/public"))
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(sessions({
-    secret: process.env.SECRET ,
-    saveUninitialized:true,
+    secret: process.env.SECRET,
+    saveUninitialized: true,
     resave: false
 }));
 app.use(express.json()); // parsing the incoming data
-// cookie parser middleware
+
+
 let session;
-let valid = false
+let valid = false // flag for checking password
+
+
+var serviceAccount = require("./config.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.DATABASEURL
+});
+const db = admin.firestore()
 
 
 // const favicon = require('serve-favicon');
@@ -33,182 +43,359 @@ const deficient = require('./data/nutriDefi.json')
 const enrich = require('./data/nutriEnrich.json')
 const teamInfo = require('./data/info.json')
 
-app.get("/", function(req, res) {
-    res.render("home", { data: homePage })
+
+// middleware functions  
+var sessionChecker = (req, res, next) => {
+    if (req.session.userid) {
+        next();
+    } else {
+        res.render('login', { invalid: valid });
+    }
+};
+
+var sessionCheckerFarmer = (req, res, next) => {
+    if (req.session.userid) {
+        if (req.session.role == 'farmer') next();
+        else res.send("UNAUTHORIZED")
+    } else {
+        res.render('login', { invalid: valid });
+    }
+};
+
+
+var sessionCheckerExpert = (req, res, next) => {
+    if (req.session.userid) {
+        if (req.session.role == 'expert') next();
+        else res.send("UNAUTHORIZED")
+    } else {
+        res.render('login', { invalid: valid });
+    }
+};
+
+
+// routes
+app.get("/", function (req, res) {
+    session = req.session;
+    const isLoggedIn = session.userid ? true : false
+    let role = "farmer"
+    if (isLoggedIn) role = session.role
+    res.render("home", { data: homePage, isLoggedIn, role })
 })
 
-app.get("/qgisPage", function(req, res) {
-    res.render("index")
+app.get("/qgisPage", sessionCheckerFarmer, function (req, res) {
+    res.render("qgisTracking")
 })
 
-
-app.get("/CropPrediction", function(req, res) {
+app.get("/CropPrediction", sessionCheckerFarmer, function (req, res) {
     res.render("CropPrediction", { info: "Fill out form" })
 })
 
-app.post("/CropPrediction", async function(req, res) {
-     
-    params = ["season", "temp", "humi", "rainfall", "soil"]  
-    obj = {} 
-
-    for(key of params ){
-        obj[key] = req.body[key]
+app.post("/CropPrediction", sessionCheckerFarmer, async function (req, res) {
+ 
+    params = ["nitrogen", "phosphorus", "potassium", "temp", "humi" , "pH" , "rainfall"]
+    obj = {
+        formVals: [],
+        marketVals: {}
     }
+
+    for (key of params) { obj.formVals.push(parseFloat(req.body[key])) }
+
+    const marketRef = await db.collection("market requirements").doc("Dy9MV98vtc33sJbhKYHt").get()
+    const crops = marketRef.data()
      
-    const rawResponse = await fetch(`https://smart-web-apis.herokuapp.com/${process.env.PATH1}`, {
+    for (const crop in crops) {
+        obj.marketVals[crop] = parseInt(crops[crop])
+    }
+
+    const rawResponse = await fetch(" http://127.0.0.1:5000/CropPred", {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(obj)
-      });
+    });
     const content = await rawResponse.json();
-    
-    res.render("CropPrediction", { info: "Cultivating '" + content.val +"' would earn you profits!" })  
+    const {res1 , res2 , res3 } = content
+    const val = `${res1}, ${res2} or ${res3}`
+    res.render("CropPrediction", { info: "Cultivating '" + val + "' would earn you profits!" })
 })
 
-
-
-app.get("/PrecisionAgriculture", function(req, res) {
+app.get("/PrecisionAgriculture", sessionCheckerFarmer, function (req, res) {
     res.render("precisionAgri", { data: precisionPage, links: {} })
 })
 
-app.post("/PrecisionAgriculture", function(req, res) {
-     
+app.post("/PrecisionAgriculture", function (req, res) {
     res.render("precisionAgri", { data: precisionPage, links: precisionPage[req.body.crop] })
 })
 
+app.get("/SmartFarming", sessionCheckerFarmer, function (req, res) {
+    session = req.session;
 
- 
-
-
-app.get("/SmartFarming", function(req, res) {
-    session=req.session; 
-    
-    if(session.userid){
-        res.render("smartFarm" , {nutriThresholds : nutriThresholds } )
-    }else{
-        res.render('login' , {invalid : valid})
+    if (session.userid) {
+        res.render("smartFarm", { nutriThresholds: nutriThresholds })
+    } else {
+        res.render('login', { invalid: valid })
     }
-    
-}) 
-
- 
-
-app.post("/login", function(req, res) {
-    const {userN , passW} = req.body
-    
-    if(passW == process.env.PASSWORD && userN == process.env.WEBNAME){
-        session=req.session;
-        session.userid=userN;
-         
-    } 
-    valid = true
-    res.redirect(301, "/SmartFarming")
 })
 
+app.get("/login", sessionChecker , function (req, res) {
+    // just a route middleware will take care of res
+})
 
-app.get('/logout',(req,res) => {
+app.post("/login", function (req, res) {
+    const { userN, passW } = req.body
+
+    // db.collection("USER").doc(`${userN}`).get()
+    //     .then(doc => {
+
+    //         if (doc.exists && doc.data().Password == passW) {
+
+    //             session = req.session;
+    //             session.userid = doc.id;
+    //             session.role = doc.data().Role;
+    //             if (doc.data().Role == 'farmer') res.redirect(301, "/SmartFarming")
+    //             else if (doc.data().Role == 'expert') res.redirect(301, "/appointment")
+    //         } else {
+    //             valid = true
+    //             res.render('login', { invalid: valid })
+    //         }
+
+    //     })
+    //     .catch(e => { console.log("ERROR") })
+
+    if (userN == '1001' && passW == 'Pass') {
+        session = req.session;
+        session.userid = userN;
+        session.role = "farmer" ;
+        res.redirect(301, "/SmartFarming")
+    } else {
+        valid = true
+        res.render('login', { invalid: valid })
+    }
+})
+
+app.get('/logout', (req, res) => {
     valid = false
     req.session.destroy();
     res.redirect('/');
 });
 
-app.get("/Weather", function(req, res) {
-    res.render("weather" , {weather : "Weather" , info: "" , api:process.env.WEATHERAPI})
+app.get("/Weather", sessionCheckerFarmer, function (req, res) {
+    res.render("weather", { weather: "Weather", info: "", api: process.env.WEATHERAPI })
 })
 
+app.post("/weatherPred", sessionCheckerFarmer, async function (req, res) {
 
-app.post("/weatherPred", async function(req, res) {
+    const { url } = req.body
     
-    const {url} = req.body 
-    params = ["temp", "humi", "press" , "visibility","windS", "windD" ]
-    obj = {}
-    for(key of params ){
-        obj[key] = req.body[key]
+    params = ["temp", "humi", "windS", "press", "windD", "visibility"]
+    obj = {
+        formVals : []
     }
-     
-    const rawResponse = await fetch(`https://smart-web-apis.herokuapp.com/${process.env.PATH2}`, {
+    for (key of params) {
+        obj["formVals"].push(parseFloat(req.body[key]))
+    }
+    
+    const rawResponse = await fetch("http://127.0.0.1:5000/weatherPred", {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(obj)
-      });
+    });
     const content = await rawResponse.json();
-      
-    res.render("weather" , {weather : "Weather" , info: content.val , url:url , api:process.env.WEATHERAPI} )
+    
+    res.render("weather", { weather: "Weather", info: content.val, url: url, api: process.env.WEATHERAPI })
 })
 
-app.get("/currConditions/:cty" , async function(req , res){
-    const val = req.params.cty  
-     
+app.get("/currConditions/:cty", async function (req, res) {
+    const val = req.params.cty
     const resp = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI}&q=${val}&aqi=no`)
-    const data = await resp.json(); //extract JSON from the http response
-     
+    const data = await resp.json(); 
     res.json(data)
 })
 
-app.get("/soil/:frt", function(req, res) {
+app.get("/soil/:frt", function (req, res) {
     const frt = req.params.frt
-     
+
     res.json({
-        Thresholds : nutriThresholds[frt] , 
-        fertilizer : fertilizer[frt] , 
-        deficient : deficient  , 
-        enrich : enrich
+        Thresholds: nutriThresholds[frt],
+        fertilizer: fertilizer[frt],
+        deficient: deficient,
+        enrich: enrich
     })
 })
 
-app.get("/firebase", async function(req, res) {
-    const resp = await fetch(`https://smart-agriot-default-${process.env.FIREBASECODE}.firebaseio.com/.json`)
-    const data = await resp.json(); //extract JSON from the http response
-    
-    res.json(data)
+app.get("/firebase", sessionCheckerFarmer , async function (req, res) {
+
+    session = req.session;
+     
+     db.collection("Farm").doc(`${session.userid}`).get()
+        .then(doc => {
+ 
+            if (doc.exists) {
+                farmData = doc.data()
+                const data = {
+                    Humidity : farmData.humid , 
+                    Temperature : farmData.temp  
+                }
+                 
+                res.json(data)
+            }  
+        })
+        .catch(e => { console.log("ERROR") })
 })
 
-app.get("/firebase/:sValve/:state", async function(req, res) {
-    session=req.session; 
-    
-    if(!session.userid){
+app.get("/firebase/:sValve/:state", async function (req, res) {
+    session = req.session;
+
+    if (!session.userid) {
         res.redirect("/SmartFarming")
-    } 
+    }
 
     obj = {}
     state = req.params.state
-    if(req.params.sValve === "mode"){
+    if (req.params.sValve === "mode") {
         obj["mode"] = state
         obj["p1"] = "0"
         obj["p2"] = "0"
         obj["p3"] = "0"
-    }else{
+    } else {
         valve = parseInt(req.params.sValve)
-        if(valve === 1){ obj["p1"] = state}
-        else if(valve === 2){ obj["p2"] = state}
-        if(valve === 3){ obj["p3"] = state}
+        if (valve === 1) { obj["p1"] = state }
+        else if (valve === 2) { obj["p2"] = state }
+        if (valve === 3) { obj["p3"] = state }
     }
-    
-    const resp = await fetch(`https://smart-agriot-default-${process.env.FIREBASECODE}.firebaseio.com/.json` , {
+
+    const resp = await fetch(`https://smart-agriot-default-${process.env.FIREBASECODE}.firebaseio.com/.json`, {
         method: 'PATCH',
         body: JSON.stringify(obj)
     })
-    const data = await resp.json(); //extract JSON from the http response
-     
-    res.json(data)
- 
-})
-
-
-
-
-app.get("/abt", function(req, res) {
-res.render("about" , {info : teamInfo})
-})
-
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Running on Port 3000")
+    const data = await resp.json();  
     
+    res.json(data)
+})
+
+app.get("/plantDoctor", sessionCheckerFarmer, async function (req, res) {
+    session = req.session;
+    const queryRef = db.collection('Consultation');
+    const doc = await queryRef.where('farmer_id', '==', session.userid).get();
+
+    let alreadyRaised = false
+    let prev = []
+
+    doc.forEach(d => {
+
+        if (d.data().status == 'solved') { prev.push(d.data()) }
+        else { alreadyRaised = d.data() }
+    })
+
+    res.render("diseaseDetect", { alreadyRaised, prev })
+})
+
+app.post("/raiseReq", sessionCheckerFarmer, function (req, res) {
+
+    session = req.session;
+
+    const d = new Date();
+    const data = {
+        farmer_id: session.userid,
+        date_generate: `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`,
+        status: "generated",
+        date_solved: "",
+        remark: ''
+    }
+    if (req.body.URL) data.image = req.body.URL
+
+    db.collection("Consultation").doc().set(data)
+        .then(() => res.json({ status: 200 }))
+        .catch(() => res.json({ status: 503 }))
+})
+
+app.get("/appointment", sessionCheckerExpert, async function (req, res) {
+
+    const queryRef = db.collection('Consultation');
+    const userRef = db.collection('USER');
+    const doc = await queryRef.where('status', '==', "generated").get();
+    let query = []
+
+    doc.forEach(async (d) => {
+
+        document = d.data()
+        document.docID = d.id
+        query.push(document)
+    })
+    for (let q of query) {
+        const userinfo = await userRef.doc(q.farmer_id).get();
+        q.name = userinfo.data().Name
+    }
+
+    res.render("expert/appointment", { query })
+})
+
+app.get("/specific/:userID", sessionCheckerExpert, async function (req, res) {
+
+    const userID = req.params.userID
+
+    const farmRef = db.collection('Farm');
+    const farmInfo = await farmRef.doc(userID).get();
+
+    if (!farmInfo.exists) {
+        res.send("Error 404 : NO USER FOUND")
+        return
+    }
+    const farmData = farmInfo.data()
+
+    const queryRef = db.collection('Consultation');
+    const queryInfo = await queryRef.where("farmer_id", "==", userID).get();
+    let currQuery = []
+    let prevQuery = []
+
+    queryInfo.forEach((d) => {
+        document = d.data()
+        document.docID = d.id
+        if (document.status == 'solved') prevQuery.push(document)
+        else currQuery.push(document)
+    })
+
+    res.render("expert/specificUser", { farmData, currQuery, prevQuery })
+})
+
+// app.get("/resolveQ/:docID", sessionCheckerExpert, async function (req, res) {
+
+//     session = req.session 
+//     if(session.role !== 'expert') {
+//         res.send("Authorization error")
+//         return 
+//     }
+//     console.log(req.params.docID)
+
+
+//     const queryRef = db.collection('Consultation').doc(req.params.docID);
+
+//     const res = await queryRef.update({status : "solved " ,}).get();
+//     let query = []
+
+//     doc.forEach(async (d) => {
+
+//         document = d.data() 
+//         document.docID = d.id 
+//         query.push(document)
+//     })
+//     for (let q of query) {
+//         const userinfo = await userRef.doc(q.farmer_id).get();
+//         q.name = userinfo.data().Name
+//     }
+//     console.log(query)
+//     res.render("expert/appointment", { query })
+// })
+
+app.get("/abt", function (req, res) {
+    res.render("about", { info: teamInfo })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Running on Port 3000")
 })
